2f0fd31bf6343c98c0e342452e3ffc615a316176
张亚飞 <672233580@qq.com>
2022-12-16 18:12:02 +0800

    Initial Commit

---
+++ IntroAnimation1216/IntroAnimation1216App.swift
@@ -0,0 +1,17 @@
+//
+//  IntroAnimation1216App.swift
+//  IntroAnimation1216
+//
+//  Created by 张亚飞 on 2022/12/16.
+//
+
+import SwiftUI
+
+@main
+struct IntroAnimation1216App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
